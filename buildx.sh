b00941e3a1a195abd8a43c49357957c9ff3686c7
jay <jay@1bcb0b09-b424-0410-92a4-e5ef3db1a8a0>
2007-11-11 04:19:34 +0000

    check package versions
    git-svn-id: svn://server1.xrdp.org/srv/svn/repos/main/x11rdp_xorg71@207 1bcb0b09-b424-0410-92a4-e5ef3db1a8a0

--- buildx.sh
+++ buildx.sh
@@ -16,7 +16,7 @@ fi
 export PKG_CONFIG_PATH=$BASEDIR/builddir/pkg-config/lib/pkgconfig
 
 # xf86dri
-pkg-config --exists 'xf86driproto'
+pkg-config --exists 'xf86driproto >= 2.0.3'
 if ! test $? -eq 0
 then
   cd xf86driproto-X11R7.0-2.0.3
@@ -29,10 +29,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xf86driproto"
 fi
 
 # glproto
-pkg-config --exists 'glproto'
+pkg-config --exists 'glproto >= 1.4.7'
 if ! test $? -eq 0
 then
   cd glproto-X11R7.1-1.4.7
@@ -45,10 +47,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping glproto"
 fi
 
 # randrproto
-pkg-config --exists 'randrproto'
+pkg-config --exists 'randrproto >= 1.1.2'
 if ! test $? -eq 0
 then
   cd randrproto-X11R7.0-1.1.2
@@ -61,10 +65,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping randrproto"
 fi
 
 # renderproto
-pkg-config --exists 'renderproto'
+pkg-config --exists 'renderproto >= 0.9.2'
 if ! test $? -eq 0
 then
   cd renderproto-X11R7.0-0.9.2
@@ -77,10 +83,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping renderproto"
 fi
 
 # fixesproto
-pkg-config --exists 'fixesproto'
+pkg-config --exists 'fixesproto >= 4.0'
 if ! test $? -eq 0
 then
   cd fixesproto-X11R7.1-4.0
@@ -93,10 +101,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping fixesproto"
 fi
 
 # damageproto
-pkg-config --exists 'damageproto'
+pkg-config --exists 'damageproto >= 1.0.3'
 if ! test $? -eq 0
 then
   cd damageproto-X11R7.0-1.0.3
@@ -109,10 +119,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping damageproto"
 fi
 
 # xcmiscproto
-pkg-config --exists 'xcmiscproto'
+pkg-config --exists 'xcmiscproto >= 1.1.2'
 if ! test $? -eq 0
 then
   cd xcmiscproto-X11R7.0-1.1.2
@@ -125,10 +137,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xcmiscproto"
 fi
 
 # xextproto
-pkg-config --exists 'xextproto'
+pkg-config --exists 'xextproto >= 7.0.2'
 if ! test $? -eq 0
 then
   cd xextproto-X11R7.0-7.0.2
@@ -141,10 +155,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xextproto"
 fi
 
 # xproto
-pkg-config --exists 'xproto'
+pkg-config --exists 'xproto >= 7.0.5'
 if ! test $? -eq 0
 then
   cd xproto-X11R7.1-7.0.5
@@ -157,10 +173,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xproto"
 fi
 
 # xtrans
-pkg-config --exists 'xtrans'
+pkg-config --exists 'xtrans >= 1.0.0'
 if ! test $? -eq 0
 then
   cd xtrans-X11R7.0-1.0.0
@@ -173,10 +191,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xtrans"
 fi
 
 # xf86miscproto
-pkg-config --exists 'xf86miscproto'
+pkg-config --exists 'xf86miscproto >= 0.9.2'
 if ! test $? -eq 0
 then
   cd xf86miscproto-X11R7.0-0.9.2
@@ -189,10 +209,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xf86miscproto"
 fi
 
 # xf86vidmodeproto
-pkg-config --exists 'xf86vidmodeproto'
+pkg-config --exists 'xf86vidmodeproto >= 2.2.2'
 if ! test $? -eq 0
 then
   cd xf86vidmodeproto-X11R7.0-2.2.2
@@ -205,10 +227,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xf86vidmodeproto"
 fi
 
 # xf86bigfontproto
-pkg-config --exists 'xf86bigfontproto'
+pkg-config --exists 'xf86bigfontproto >= 1.1.2'
 if ! test $? -eq 0
 then
   cd xf86bigfontproto-X11R7.0-1.1.2
@@ -221,10 +245,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xf86bigfontproto"
 fi
 
 # scrnsaverproto
-pkg-config --exists 'scrnsaverproto'
+pkg-config --exists 'scrnsaverproto >= 1.1.0'
 if ! test $? -eq 0
 then
   cd scrnsaverproto-X11R7.1-1.1.0
@@ -237,10 +263,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping scrnsaverproto"
 fi
 
 # bigreqsproto
-pkg-config --exists 'bigreqsproto'
+pkg-config --exists 'bigreqsproto >= 1.0.2'
 if ! test $? -eq 0
 then
   cd bigreqsproto-X11R7.0-1.0.2
@@ -253,10 +281,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping bigreqsproto"
 fi
 
 # resourceproto
-pkg-config --exists 'resourceproto'
+pkg-config --exists 'resourceproto >= 1.0.2'
 if ! test $? -eq 0
 then
   cd resourceproto-X11R7.0-1.0.2
@@ -269,10 +299,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping resourceproto"
 fi
 
 # fontsproto
-pkg-config --exists 'fontsproto'
+pkg-config --exists 'fontsproto >= 2.0.2'
 if ! test $? -eq 0
 then
   cd fontsproto-X11R7.0-2.0.2
@@ -285,10 +317,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping fontsproto"
 fi
 
 # inputproto
-pkg-config --exists 'inputproto'
+pkg-config --exists 'inputproto >= 1.3.2'
 if ! test $? -eq 0
 then
   cd inputproto-X11R7.0-1.3.2
@@ -301,10 +335,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping inputproto"
 fi
 
 # xf86dgaproto
-pkg-config --exists 'xf86dgaproto'
+pkg-config --exists 'xf86dgaproto >= 2.0.2'
 if ! test $? -eq 0
 then
   cd xf86dgaproto-X11R7.0-2.0.2
@@ -317,10 +353,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xf86dgaproto"
 fi
 
 # videoproto
-pkg-config --exists 'videoproto'
+pkg-config --exists 'videoproto >= 2.2.2'
 if ! test $? -eq 0
 then
   cd videoproto-X11R7.0-2.2.2
@@ -333,10 +371,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping videoproto"
 fi
 
 # compositeproto
-pkg-config --exists 'compositeproto'
+pkg-config --exists 'compositeproto >= 0.3.1'
 if ! test $? -eq 0
 then
   cd compositeproto-X11R7.1-0.3.1
@@ -349,10 +389,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping compositeproto"
 fi
 
 # trapproto
-pkg-config --exists 'trapproto'
+pkg-config --exists 'trapproto >= 3.4.3'
 if ! test $? -eq 0
 then
   cd trapproto-X11R7.0-3.4.3
@@ -365,10 +407,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping trapproto"
 fi
 
 # recordproto
-pkg-config --exists 'recordproto'
+pkg-config --exists 'recordproto >= 1.13.2'
 if ! test $? -eq 0
 then
   cd recordproto-X11R7.0-1.13.2
@@ -381,10 +425,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping recordproto"
 fi
 
 # xineramaproto
-pkg-config --exists 'xineramaproto'
+pkg-config --exists 'xineramaproto >= 1.1.2'
 if ! test $? -eq 0
 then
   cd xineramaproto-X11R7.0-1.1.2
@@ -397,10 +443,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xineramaproto"
 fi
 
 # ice
-pkg-config --exists 'ice'
+pkg-config --exists 'ice >= 1.0.1'
 if ! test $? -eq 0
 then
   cd libICE-X11R7.1-1.0.1
@@ -413,10 +461,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping ice"
 fi
 
 # sm
-pkg-config --exists 'sm'
+pkg-config --exists 'sm >= 1.0.1'
 if ! test $? -eq 0
 then
   cd libSM-X11R7.1-1.0.1
@@ -429,10 +479,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping sm"
 fi
 
 # xau
-pkg-config --exists 'xau'
+pkg-config --exists 'xau >= 1.0.1'
 if ! test $? -eq 0
 then
   cd libXau-X11R7.1-1.0.1
@@ -445,10 +497,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xau"
 fi
 
 # kbproto
-pkg-config --exists 'kbproto'
+pkg-config --exists 'kbproto >= 1.0.2'
 if ! test $? -eq 0
 then
   cd kbproto-X11R7.0-1.0.2
@@ -461,10 +515,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping kbproto"
 fi
 
 # Xdmcp
-pkg-config --exists 'xdmcp'
+pkg-config --exists 'xdmcp >= 1.0.1'
 if ! test $? -eq 0
 then
   cd libXdmcp-X11R7.1-1.0.1
@@ -477,10 +533,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xdmcp"
 fi
 
 # x11
-pkg-config --exists 'x11'
+pkg-config --exists 'x11 >= 1.0.1'
 if ! test $? -eq 0
 then
   cd libX11-X11R7.1-1.0.1
@@ -493,10 +551,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping x11"
 fi
 
 # xt
-pkg-config --exists 'xt'
+pkg-config --exists 'xt >= 1.0.2'
 if ! test $? -eq 0
 then
   cd libXt-X11R7.1-1.0.2
@@ -509,10 +569,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xt"
 fi
 
 # evieext evieproto
-pkg-config --exists 'evieproto'
+pkg-config --exists 'evieproto >= 1.0.2'
 if ! test $? -eq 0
 then
   cd evieext-X11R7.0-1.0.2
@@ -525,10 +587,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping evieproto"
 fi
 
 # xkbfile
-pkg-config --exists 'xkbfile'
+pkg-config --exists 'xkbfile >= 1.0.3'
 if ! test $? -eq 0
 then
   cd libxkbfile-X11R7.1-1.0.3
@@ -541,10 +605,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xkbfile"
 fi
 
 # fontcacheproto
-pkg-config --exists 'fontcacheproto'
+pkg-config --exists 'fontcacheproto >= 0.1.2'
 if ! test $? -eq 0
 then
   cd fontcacheproto-X11R7.0-0.1.2
@@ -557,10 +623,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping fontcacheproto"
 fi
 
 # fontenc
-pkg-config --exists 'fontenc'
+pkg-config --exists 'fontenc >= 1.0.2'
 if ! test $? -eq 0
 then
   cd libfontenc-X11R7.1-1.0.2
@@ -573,10 +641,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping fontenc"
 fi
 
 # xfont
-pkg-config --exists 'xfont'
+pkg-config --exists 'xfont >= 1.1.0'
 if ! test $? -eq 0
 then
   cd libXfont-X11R7.1-1.1.0
@@ -589,10 +659,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xfont"
 fi
 
 # Xext
-pkg-config --exists 'xext'
+pkg-config --exists 'xext >= 1.0.1'
 if ! test $? -eq 0
 then
   cd libXext-X11R7.1-1.0.1
@@ -605,10 +677,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xext"
 fi
 
 # xmu
-pkg-config --exists 'xmu'
+pkg-config --exists 'xmu >= 1.0.1'
 if ! test $? -eq 0
 then
   cd libXmu-X11R7.1-1.0.1
@@ -621,10 +695,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xmu"
 fi
 
 # xkbui
-pkg-config --exists 'xkbui'
+pkg-config --exists 'xkbui >= 1.0.2'
 if ! test $? -eq 0
 then
   cd libxkbui-X11R7.1-1.0.2
@@ -637,10 +713,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xkbui"
 fi
 
 # xxf86misc
-pkg-config --exists 'xxf86misc'
+pkg-config --exists 'xxf86misc >= 1.0.1'
 if ! test $? -eq 0
 then
   cd libXxf86misc-X11R7.1-1.0.1
@@ -653,10 +731,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xxf86misc"
 fi
 
 # xxf86vm
-pkg-config --exists 'xxf86vm'
+pkg-config --exists 'xxf86vm >= 1.0.1'
 if ! test $? -eq 0
 then
   cd libXxf86vm-X11R7.1-1.0.1
@@ -669,10 +749,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xxf86vm"
 fi
 
 # xpm
-pkg-config --exists 'xpm'
+pkg-config --exists 'xpm >= 3.5.5'
 if ! test $? -eq 0
 then
   cd libXpm-X11R7.1-3.5.5
@@ -685,10 +767,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xpm"
 fi
 
 # xaw7
-pkg-config --exists 'xaw7'
+pkg-config --exists 'xaw7 >= 1.0.2'
 if ! test $? -eq 0
 then
   cd libXaw-X11R7.1-1.0.2
@@ -701,10 +785,12 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping xaw7"
 fi
 
 # libdrm - special
-pkg-config --exists 'libdrm'
+pkg-config --exists 'libdrm >= 2.3.0'
 if ! test $? -eq 0
 then
   cd libdrm-2.3.0
@@ -717,22 +803,20 @@ then
   make
   make install
   cd ..
+else
+  echo "skipping libdrm"
 fi
 
 # xorg-server
-pkg-config --exists 'xorg-server'
+cd xorg-server-X11R7.1-1.1.0
+./configure --prefix=$PREFIXDIR --enable-xglx --with-mesa-source=$BASEDIR/Mesa-6.5
 if ! test $? -eq 0
 then
-  cd xorg-server-X11R7.1-1.1.0
-  ./configure --prefix=$PREFIXDIR --enable-xglx --with-mesa-source=$BASEDIR/Mesa-6.5
-  if ! test $? -eq 0
-  then
-    echo "error xorg-server"
-    exit 1;
-  fi
-  make
-  make install
-  cd ..
+  echo "error xorg-server"
+  exit 1;
 fi
+make
+make install
+cd ..
 
 echo "all ok"
